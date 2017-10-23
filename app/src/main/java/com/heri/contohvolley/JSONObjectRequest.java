dc580012c0dd034ee15c74349dd138c4f45530d5
Heri Prastio <heri.prastio56@gmail.com>
2017-10-23 16:23:09 +0700

    First commit

---
+++ app/src/main/java/com/heri/contohvolley/JSONObjectRequest.java
@@ -0,0 +1,19 @@
+//package com.heri.contohvolley;
+//
+//import com.android.volley.Request;
+//import com.android.volley.Response;
+//import com.android.volley.toolbox.StringRequest;
+//
+///**
+// * Created by heri on 10/23/17.
+// */
+//
+//public class JSONObjectRequest {
+//    String url = "http://www.google.com";
+//    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, null, new Response.Listener<String>() {
+//        @Override
+//        public void onResponse(String response) {
+//
+//        }
+//    })
+//}
---
+++ app/src/main/java/com/heri/contohvolley/MainActivity.java
@@ -0,0 +1,42 @@
+package com.heri.contohvolley;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+//    final TextView tv= (TextView) findViewById(R.id.Tview);
+TextView tv;
+//    String url ="http://www.google.com";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.Tview);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http:localhost";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                tv.setText("Response adalah : " + response.substring(0, 10000));
+                findViewById(R.id.Tview);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tv.setText("Volley Error, Tidak work");
+            }
+        });
+        queue.add(stringRequest);
+    }
+}
